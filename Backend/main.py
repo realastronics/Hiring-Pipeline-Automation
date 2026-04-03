f379b183a81e65f1281f9fa75ae122b68c679280
Parth <parthsharma0206@gmail.com>
2026-04-03 09:36:00 +0530

    the butter

--- Backend/main.py
+++ Backend/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"],    
 )
 
 @app.get("/")
